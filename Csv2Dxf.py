966bfb083b1a51c0b47e7792c5806c1a34935136
treenester <treenester@gmail.com>
2024-05-30 16:07:24 -0500

    using dxfwrite (outdated)

--- Csv2Dxf.py
+++ Csv2Dxf.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python
 
-import argparse
 import csv
-import pandas
+import dxfwrite
+
+import argparse
+#import pandas
 
 __author__ = 'dlb'
+__version__ = '2.0'
  
-parser = argparse.ArgumentParser(description='Csv to Dxf Conversion, 1.0')
+parser = argparse.ArgumentParser(description='Csv to Dxf Conversion')
 parser.add_argument('-i','--input', help='infile',required=True)
 parser.add_argument('-o','--output',help='outfile', required=True)
 args = parser.parse_args()
@@ -18,111 +21,37 @@ print ("Output file: %s" % args.output )
 ## process files ##
 
 # create/overwrite output file
-outfile = open(args.output, "w");
-outfile.write('  0\n')
-outfile.write('SECTION\n')
-outfile.write('  2\n')
-outfile.write('ENTITIES\n')
-outfile.close()
+# and start entities section
+from dxfwrite import DXFEngine as dxf
+drawing = dxf.drawing(args.output)
+drawing.add_layer('PT')
+drawing.add_layer('PN', color=2)
+drawing.add_layer('EL', color=3)
+drawing.add_layer('DE', color=4)
+drawing.save()
 
 # open csv file and process contents
-reader = csv.reader(open(args.input))
-for pnum, northing, easting, elevation, desc in reader:
-	outfile = open(args.output, "a");
-	outfile.write('  0\n')
-	outfile.write('POINT\n')
-	outfile.write('  8\n')
-	outfile.write('PT\n')
-	outfile.write(' 62\n')
-	outfile.write('     7\n')
-	outfile.write(' 10\n')
-	outfile.write(str(easting) + '\n')
-	outfile.write(' 20\n')
-	outfile.write(str(northing) + '\n')
-	outfile.write(' 30\n')
-	outfile.write(str(elevation) + '\n')
-	outfile.write('  0\n')
-	outfile.write('TEXT\n')
-	outfile.write('  8\n')
-	outfile.write('PN\n')
-	outfile.write(' 62\n')
-	outfile.write('     2\n')
-	outfile.write(' 10\n')
-	outfile.write(str(easting) + '\n')
-	outfile.write(' 20\n')
-	outfile.write(str(northing) + '\n')
-	outfile.write(' 30\n')
-	outfile.write('0\n')
-	outfile.write(' 40\n')
-	outfile.write('0.1\n')
-	outfile.write('  1\n')
-	outfile.write(str(pnum) + '\n')
-	outfile.write(' 72\n')
-	outfile.write('     0\n')
-	outfile.write(' 11\n')
-	outfile.write(str(easting) + '\n')
-	outfile.write(' 21\n')
-	outfile.write(str(northing) + '\n')
-	outfile.write(' 31\n')
-	outfile.write('0\n')
-	outfile.write(' 73\n')
-	outfile.write('     1\n')
-	outfile.write('  0\n')
-	outfile.write('TEXT\n')
-	outfile.write('  8\n')
-	outfile.write('EL\n')
-	outfile.write(' 62\n')
-	outfile.write('     4\n')
-	outfile.write(' 10\n')
-	outfile.write(str(easting) + '\n')
-	outfile.write(' 20\n')
-	outfile.write(str(northing) + '\n')
-	outfile.write(' 30\n')
-	outfile.write('0\n')
-	outfile.write(' 40\n')
-	outfile.write('0.1\n')
-	outfile.write('  1\n')
-	outfile.write(str(elevation) + '\n')
-	outfile.write('72\n')
-	outfile.write('  2\n')
-	outfile.write(' 11\n')
-	outfile.write(str(easting) + '\n')
-	outfile.write(' 21\n')
-	outfile.write(str(northing) + '\n')
-	outfile.write(' 31\n')
-	outfile.write('0\n')
-	outfile.write(' 73\n')
-	outfile.write('     3\n')
-	outfile.write('  0\n')
-	outfile.write('TEXT\n')
-	outfile.write('  8\n')
-	outfile.write('DE\n')
-	outfile.write(' 62\n')
-	outfile.write('     3\n')
-	outfile.write(' 10\n')
-	outfile.write(str(easting) + '\n')
-	outfile.write(' 20\n')
-	outfile.write(str(northing) + '\n')
-	outfile.write(' 30\n')
-	outfile.write('0\n')
-	outfile.write(' 40\n')
-	outfile.write('0.1\n')
-	outfile.write('  1\n')
-	outfile.write(str(desc) + '\n')
-	outfile.write('72\n')
-	outfile.write('  0\n')
-	outfile.write(' 11\n')
-	outfile.write(str(easting) + '\n')
-	outfile.write(' 21\n')
-	outfile.write(str(northing) + '\n')
-	outfile.write(' 31\n')
-	outfile.write('0\n')
-	outfile.write(' 73\n')
-	outfile.write('     3\n')
+with open(args.input) as file_obj:
+    reader = csv.reader(file_obj)
+#df = pandas.read_csv(open(args.input))
+    for pnum, north, east, elev, desc in reader:
+        print ("Processing point: " + pnum)
+        
+        pt = dxf.point((east, north, elev))
+        pt['layer'] = 'PT'
+        drawing.add(pt)
 
-outfile.write('  0\n')
-outfile.write('ENDSEC\n')
-outfile.write('  0\n')
-outfile.write('EOF\n')
-outfile.close()
+        pn = dxf.text(pnum, (east, north), valign=dxfwrite.BOTTOM, alignpoint=(east, north))
+        pn['layer'] = 'PN'
+        drawing.add(pn)
+
+        el = dxf.text(str(elev), (east, north), halign=dxfwrite.RIGHT, valign=dxfwrite.BOTTOM, alignpoint=(east, north))
+        el['layer'] = 'EL'
+        drawing.add(el)
+
+        de = dxf.text(desc, (east, north), valign=dxfwrite.TOP, alignpoint=(east, north))
+        de['layer'] = 'DE'
+        drawing.add(de)
+
+drawing.save()
 print ("Done\n")
